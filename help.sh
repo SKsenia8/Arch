5d7f55ea9806153dfb19f9255a00cfd587626dd5
KS <ksstar200462@gmail.com>
2024-10-20 18:33:02 +0300

    Help scripts

---
+++ help.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+#программа принимает строки вида -n 1 -m 2 -p 0.3 -b backup_directory
+help() { #сообщение вывод основной информации
+    echo -e "\e[1mUsage:\e[0m $0 [OPTIONS] [DIRECTORY]" 
+    echo
+    echo -e "\e[1mArguments:\e[0m"
+    echo "[DIRECTORY] [default: .]"
+    echo
+    echo -e "\e[1mOptions:\e[0m"
+    echo -e "\e[1m-n\e[0m\t\tNumber of files to compress"
+    echo -e "\e[1m-m\e[0m\t\tMax size of directory in GB"
+    echo -e "\e[1m-p\e[0m\t\tPercentage to compress in [0.1 .. 1]"
+    echo -e "\e[1m-b\e[0m\t\tBackup directory (default: .)"
+    echo -e "\e[1m-h\e[0m\t\tPrint this message"
+}
+
+#тут ксюша должна была добавить названия свои директорий
+directory=.
+backup_directory=.
+
+#цикл. проходится по строке, собирает информацию
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -n)
+            N=$2 #  количество файлов
+           
+            shift 2;; # сдвиг дальше. след команда
+        -m)
+            gb=$2 #количество гигабайтов
+            maxsize=$(bc <<< "$gb * 1024 * 1024 * 1024")  #перевод в байты
+            shift 2;;
+        -p)
+            per=$2 #переменная указанный процент именно в формате дроби
+            if [[ $(bc <<< "$per <= 0 || $per > 1.0") -eq 1 ]]; then
+                echo "-p argument must be in [0.1 .. 1]"
+                exit 1
+            fi
+            shift 2;;
+        -b)
+            backup_directory=$2 #указание бэкап директории. проверка ее существовани
+            if [[ ! -d "$backup_directory" ]]; then
+                echo "Backup directory $backup_directory does not exist"
+                exit 1
+            fi
+            shift 2;;
+        -h) #вывод сообщения помощи
+            help
+            exit;;
+        -*) 
+            echo "Unknown option: $1"
+            exit 1;;
+        *)
+            directory=$1
+            shift;;
+    esac
+done #завершение цикла
+
+total_size=$(du -sb $directory | cut -f 1)  #размер директории
+
+files=$(ls -tr $directory)  #файлы, сортируя их по времени изменения в обратном порядке (самые старые файлы вверху) t - time r обратный порядок
+
+if [[ $(bc -l <<< "($total_size / $maxsize) >= $per") -eq 1 ]]; #проверка, если папка занята больше чем процент 
+then
+    echo -e "Directory size exceeds \e[1m$per\e[0m of maximum size"
+    i=0
+    files_to_compress=()
+    cd $directory
+    for file in $files #процесс сжатия файлов
+    do
+        files_to_compress+=($file)
+        i=$((i+1)) 
+        if [[ $i -eq $N ]]; then
+            break #когда I в цикле равно n введеное, стоп, нужно сжать n файлов
+        fi
+    done
+    # архивация в бэкап директорию
+    echo -e "\e[1mCompressing\e[0m: \e[4m\e[3m${files_to_compress[@]}\e[0m to $backup_directory/out.tar.gz"
+    tar czfv out.tar.gz ${files_to_compress[@]} 
+    rm ${files_to_compress[@]}
+    cd -
+    if [[ "$backup_directory" != "." ]]; then
+        echo -e "Backing up to $backup_directory/out.tar.gz"
+        mv $directory/out.tar.gz $backup_directory/
+    fi
+else
+    echo "Nothing to do"
+fi
+
---
+++ test2.bat
@@ -0,0 +1,67 @@
+@echo off
+SetLocal EnableDelayedExpansion
+rem Получаем путь до папки, обозначаем константы 
+set path=%1
+set /a proc=10
+set /a n = 5
+
+rem Проверяем существует ли такой путь, если нет заканчиваем выполнение
+if not exist "%path%" echo Wrong path & exit /b
+
+rem Достаем размер папки и оставшееся место на диске
+for /F "tokens=3" %%a in ('dir %path%') do echo %%a >> data.txt
+
+for /f "usebackq delims=" %%a in ("data.txt") do set "size_folder=!free_size!" & set "free_size=%%a"
+del data.txt
+
+for %%I in (%size_folder%) do set size_f=!size_f!%%I
+for %%I in (%free_size%) do set size_d=!size_d!%%I
+
+rem Ищем длину строк, которые содержат размер папки и оставшеся место на диске
+set /a len_f=0
+set /a len_d=0
+for /L %%A IN (0,1,8100) do if not "!size_f:~%%A,1!"=="" set /a len_f = %%A + 1
+for /L %%A IN (0,1,8100) do if not "!size_d:~%%A,1!"=="" set /a len_d = %%A + 1
+
+rem Преобразуем строки в числа, если число больше 32 байт записываем самое большое возможное число для этого ограничения
+if %len_d% GTR 10 set /a disk =2147483647
+if %len_d% == 10 if %size_d% GTR '2147483647' set /a disk =2147483647
+if %len_d% == 10 if %size_d% LEQ '2147483647' set /a disk =%size_d%
+if %len_d% LSS 10 set /a disk =%size_d%
+
+if %len_f% GTR 10 set /a folder =2147483647
+if %len_f% == 10 if %size_f% GTR '2147483647' set /a folder =2147483647
+if %len_f% == 10 if %size_f% LEQ '2147483647' set /a folder =%size_f%
+if %len_f% LSS 10 set /a folder =%size_f%
+
+rem Находим процент заполненности
+set /a pr = (%folder% * 100) / (%disk% + %folder%)
+
+rem Если процент меньше целевого выходим и пишем об этом пользователю
+if %pr% LEQ %proc% echo No archiving required & exit /b
+
+rem Проверяем наличие архиватора
+if not exist "C:\Program Files\7-Zip\7z.exe" echo The archiver program 7-zip is not installed at the address: C:\Program Files & exit /b
+
+rem  ищем n самых старых файлов
+for /f "tokens=*" %%a in ('dir %path% /a-d /o-d /b') do echo %%a >> data.txt
+
+set /a count_file = 0
+for /f "usebackq delims=" %%a in ("data.txt") do set /a count_file = !count_file! + 1
+
+if %count_file% == 1 echo folder is empty & exit /b
+
+set /a dif = %count_file% - %n%
+if  %dif% LSS 1 echo folder don't have enough files to archive & exit /b
+
+rem Создаем \backup и переносим туда самые старые файлы
+md %path%\backup
+for /f "usebackq delims=" %%a in ("data.txt") do set /a count_file = !count_file! - 1 & if !count_file! LSS %n% move "%path%\%%a" "%path%\backup"
+del data.txt
+
+rem Архивация файлов
+"C:\Program Files\7-Zip\7z.exe" a -mx9 %path%\archive.7z %path%\backup
+
+rem Удаление файлов, которые были заархивированы
+del /f /s /q %path%\backup
+move "%path%\archive.7z" "%path%\backup"
